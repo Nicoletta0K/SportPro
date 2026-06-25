6e3d8baad1aadad94178173db2ee3b812d8792bb
Nicoletta <nicoleartavia062@gmail.com>
2026-06-25 14:36:44 -0600

    Se creo la clase participantes
    se creo la clase participantes y sus metodos

--- src/participantes/Participante.java
+++ src/participantes/Participante.java
@@ -1,4 +1,19 @@
 package participantes;
-public class Participante {
+import java.time.LocalDate;
 import personas.Persona;
+public class Participante extends Persona {
+    private Disciplina disciplinaInscrita; 
+    public Participante(String identificacion, String Nombre, LocalDate fechaNacimiento, String telefono, Disciplina disciplinaInscrita) {
+        super(identificacion, nombre, fechaNacimiento, telefono);
+        this.disciplinaInscrita = disciplinaInscrita;
+    }
+    public Disciplina getDisciplinaInscrita() {
+        return disciplinaInscrita;
+    }
+    public void setDisciplinaInscrita(Disciplina disciplinaInscrita) {
+        this.disciplinaInscrita = disciplinaInscrita;
+    }
+    public String toString() {
+        return "Participante: " + getNombre() + " | ID: " + getIdentificacion() + " | Disciplina: " + disciplinaInscrita.getNombre();
+    }
 }
