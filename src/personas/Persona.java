f3ae942a1598ccce09f8c5d0021d6fc5622ee220
FlyStudent <anporrasnu@est.utn.ac.cr>
2026-06-25 13:53:14 -0600

    Clase persona
    se crea el archivo de la clase abstracta persona con sus atributos

---
+++ src/personas/Persona.java
@@ -0,0 +1,10 @@
+package personas;
+
+import java.time.LocalDate;
+
+public abstract class Persona {
+    protected String identificacion;
+    protected String nombre;
+    protected LocalDate fechaNacimiento;
+    public int telefono;
+}
