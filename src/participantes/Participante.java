9935fac54083602aead0f7237f7199c5c8568ea5
Nicoletta <nicoleartavia062@gmail.com>
2026-06-25 15:10:16 -0600

    Se corrigieron errores en la clase Participantes
    Se realizaron cambios en la clase Participantes

--- src/participantes/Participante.java
+++ src/participantes/Participante.java
@@ -1,18 +1,22 @@
 package participantes;
 import java.time.LocalDate;
 import personas.Persona;
+import Disciplina.DisciplinaDep; 
+
 public class Participante extends Persona {
-    private Disciplina disciplinaInscrita; 
-    public Participante(String identificacion, String Nombre, LocalDate fechaNacimiento, String telefono, Disciplina disciplinaInscrita) {
+    private DisciplinaDep disciplinaInscrita;
+   public Participante(String identificacion, String nombre, LocalDate fechaNacimiento, int telefono, DisciplinaDep disciplinaInscrita) {
         super(identificacion, nombre, fechaNacimiento, telefono);
         this.disciplinaInscrita = disciplinaInscrita;
     }
-    public Disciplina getDisciplinaInscrita() {
+    public DisciplinaDep getDisciplinaInscrita() {
         return disciplinaInscrita;
     }
-    public void setDisciplinaInscrita(Disciplina disciplinaInscrita) {
+    public void setDisciplinaInscrita(DisciplinaDep disciplinaInscrita) {
         this.disciplinaInscrita = disciplinaInscrita;
     }
+
+    @Override
     public String toString() {
         return "Participante: " + getNombre() + " | ID: " + getIdentificacion() + " | Disciplina: " + disciplinaInscrita.getNombre();
     }
